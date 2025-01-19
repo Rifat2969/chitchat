e8bb1b5ef312d75a86297a13cee15044df41859f
imtiazahammed <ahammedrifat1920@gmail.com>
2025-01-20 01:10:24 +0600

    messege

---
+++ lib/Model/chat_buddy.dart
@@ -0,0 +1,50 @@
+import 'package:chit_chat/Model/user.dart';
+
+class ChatBuddy {
+  int? id;
+  User? user;
+  String? message;
+  String? date;
+  int? timeInSec;
+
+  ChatBuddy({this.id, this.timeInSec, this.user, this.message});
+
+  ChatBuddy.fromJson(json) {
+    id = json['id'];
+    user = json['user'] != null ? User.fromJson(json['user']) : null;
+    message = json['msg'];
+    timeInSec = json['time_in_sec'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    if (user != null) map['user'] = user?.toJson();
+    map['msg'] = message;
+    map['time_in_sec'] = timeInSec;
+    return map;
+  }
+}
+
+List<ChatBuddy> CHAT_BUDDIES = [
+  ChatBuddy(id: 1, user: ALL_USERS[0], message: "Hey, how's it going?", timeInSec: 32851),
+  ChatBuddy(id: 2, user: ALL_USERS[1], message: 'Just finished a project!', timeInSec: 42921),
+  ChatBuddy(id: 3, user: ALL_USERS[2], message: 'Can we meet later?', timeInSec: 30012),
+  ChatBuddy(id: 4, user: ALL_USERS[3], message: 'I had a great weekend!', timeInSec: 62548),
+  ChatBuddy(id: 5, user: ALL_USERS[4], message: 'Working on something exciting!', timeInSec: 48101),
+  ChatBuddy(id: 6, user: ALL_USERS[5], message: 'Looking forward to our next meeting.', timeInSec: 19802),
+  ChatBuddy(id: 7, user: ALL_USERS[6], message: "Hope you're doing well!", timeInSec: 67321),
+  ChatBuddy(id: 8, user: ALL_USERS[7], message: 'Catch up later?', timeInSec: 47290),
+  ChatBuddy(id: 9, user: ALL_USERS[8], message: "What's up?", timeInSec: 54300),
+  ChatBuddy(id: 10, user: ALL_USERS[9], message: 'Got any plans for tonight?', timeInSec: 10234),
+  ChatBuddy(id: 11, user: ALL_USERS[10], message: 'How can I help you today?', timeInSec: 28132),
+  ChatBuddy(id: 12, user: ALL_USERS[11], message: 'Exciting times ahead!', timeInSec: 30013),
+  ChatBuddy(id: 13, user: ALL_USERS[12], message: "Let's collaborate!", timeInSec: 47023),
+  ChatBuddy(id: 14, user: ALL_USERS[13], message: "I'll send the details over.", timeInSec: 53821),
+  ChatBuddy(id: 15, user: ALL_USERS[14], message: 'Can we reschedule?', timeInSec: 27190),
+  ChatBuddy(id: 16, user: ALL_USERS[15], message: 'Any updates on the project?', timeInSec: 48132),
+  ChatBuddy(id: 17, user: ALL_USERS[16], message: 'Thanks for the heads up!', timeInSec: 30213),
+  ChatBuddy(id: 18, user: ALL_USERS[17], message: 'Happy to help!', timeInSec: 50832),
+  ChatBuddy(id: 19, user: ALL_USERS[18], message: "I'm wrapping up this task.", timeInSec: 67120),
+  ChatBuddy(id: 20, user: ALL_USERS[19], message: "Let's catch up tomorrow.", timeInSec: 22819),
+];
--- lib/Screen/chat_screen.dart
+++ lib/Screen/chat_screen.dart
@@ -1,4 +1,5 @@
-import 'package:chit_chat/Extention/number_ext.dart';
+import 'package:chit_chat/Model/chat_buddy.dart';
+import 'package:chit_chat/Model/user.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -8,68 +9,201 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          height: 100.height,
           padding: const EdgeInsets.all(15),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Header section
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   const Icon(
                     Icons.account_circle,
-                    size: 60,
+                    size: 40,
+                    color: Colors.grey,
                   ),
-                  SizedBox(width: 3.width),
+                  const SizedBox(width: 5),
                   const Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "Ahammed Rifat",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        "Bashundhara R/A",
-                        style: TextStyle(color: Colors.black54, fontSize: 12),
-                      )
+                      Text("Ahammed Rifat", style: TextStyle(fontSize: 12)),
+                      Text("Bashundhara R/A", style: TextStyle(color: Colors.black87, fontSize: 10)),
                     ],
                   ),
                   const Spacer(),
-                  const CircleAvatar(backgroundColor: Colors.black12, child: Icon(Icons.notifications_outlined))
+                  Container(
+                    height: 30,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black26, // Border color
+                        width: 1.0, // Border width
+                      ),
+                    ),
+                    child: const CircleAvatar(
+                      backgroundColor: Colors.black12,
+                      child: Icon(
+                        Icons.notifications_outlined,
+                        color: Colors.black45,
+                        size: 20,
+                      ),
+                    ),
+                  ),
                 ],
               ),
-              SizedBox(
-                height: 4.height,
+              const SizedBox(height: 20),
+              Text(
+                'Chat with Your Providers',
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
               ),
-              const Text(
-                "Chat With Your Providers",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, letterSpacing: .02),
-              ),
-              SizedBox(height: 2.height),
+              SizedBox(height: 10),
+              // Search bar
               TextField(
                 decoration: InputDecoration(
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
+                  prefixIcon: const Icon(Icons.search, color: Colors.grey),
                   hintText: 'Search...',
-                  hintStyle: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(vertical: 16),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 8),
                   filled: true,
                   fillColor: Colors.black12,
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
                     borderSide: BorderSide.none,
                   ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              SizedBox(
+                height: 70,
+                child: ListView.builder(
+                  itemCount: ALL_USERS.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    final controller = AnimationController(
+                      duration: const Duration(milliseconds: 800),
+                      vsync: this,
+                    );
+
+                    Future.delayed(Duration(milliseconds: index * 100), () {
+                      controller.forward();
+                    });
+
+                    return AnimatedBuilder(
+                      animation: controller,
+                      builder: (context, child) {
+                        return AnimatedOpacity(
+                          opacity: controller.value,
+                          duration: const Duration(milliseconds: 100),
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: Colors.black26,
+                                      width: 1.0,
+                                    ),
+                                  ),
+                                  child: CircleAvatar(
+                                    backgroundImage:
+                                        ALL_USERS[index].avatar != null ? NetworkImage(ALL_USERS[index].avatar!) : null,
+                                    child: ALL_USERS[index].avatar == null
+                                        ? const Icon(Icons.person, color: Colors.grey)
+                                        : null,
+                                  ),
+                                ),
+                                Text(
+                                  ALL_USERS[index].name ?? "Unknown User",
+                                  style: const TextStyle(fontSize: 12, color: Colors.black54),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+
+              Text(
+                'Recent Masseges',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: CHAT_BUDDIES.length,
+                  itemBuilder: (context, index) {
+                    final controller = AnimationController(
+                      duration: const Duration(milliseconds: 800), // Slower animation duration
+                      vsync: this,
+                    );
+
+                    Future.delayed(Duration(milliseconds: index * 100), () {
+                      controller.forward(); // Trigger animation with a delay
+                    });
+
+                    return AnimatedBuilder(
+                      animation: controller,
+                      builder: (context, child) {
+                        return AnimatedOpacity(
+                          opacity: controller.value,
+                          duration: const Duration(milliseconds: 100),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: Colors.black26, // Border color
+                                        width: 1.0, // Border width
+                                      ),
+                                    ),
+                                    child: CircleAvatar(
+                                      backgroundImage: CHAT_BUDDIES[index].user?.avatar != null
+                                          ? NetworkImage(CHAT_BUDDIES[index].user!.avatar!)
+                                          : null,
+                                      child: CHAT_BUDDIES[index].user?.avatar == null
+                                          ? const Icon(Icons.person, color: Colors.grey)
+                                          : null,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          CHAT_BUDDIES[index].user?.name ?? "Unknown User",
+                                          style: const TextStyle(fontWeight: FontWeight.w500),
+                                        ),
+                                        Text(
+                                          CHAT_BUDDIES[index].message ?? "",
+                                          style: const TextStyle(color: Colors.black54),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              if (index != CHAT_BUDDIES.length - 1) const Divider(),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
                 ),
               ),
             ],
